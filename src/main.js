e69a15f08fb4aebee439b0b0612d365022861db6
Jose Manuel Dominguez Estrada <josemanueldomingueze@gmail.com>
2021-10-04 17:01:39 -0700

    se agregaron primeros ejercicios de babel usando destinos como dist y lib para comparación de codigo y transpilacion

---
+++ src/main.js
@@ -0,0 +1,6 @@
+import { suma, saludo } from "./operaciones";
+
+// document.write(`<h2>${suma}(1,2)</h2>`)
+
+console.log(suma(1,3));
+console.log(saludo);
---
+++ src/operaciones.js
@@ -0,0 +1,13 @@
+export const suma = (num1, num2) => {
+    return num1 + num2;
+}
+
+const resta = (num1, num2) => {
+    return num1 - num2; 
+}
+
+const multiplicacion = (num1, num2) => {
+    return num1 * num2;
+}
+
+export const saludo = "HOLA, soy Manuel saludos desde un módulo";
